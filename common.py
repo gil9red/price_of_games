bb830867601caf0a7bf67cc105df83b35f5fed8e
ipetrash <ilya.petrash@inbox.ru>
2018-03-25 18:19:15 +0500

    Удаление постфикса "(DLC)" из названия игры перед сравнением

--- common.py
+++ common.py
@@ -780,10 +780,19 @@ def smart_comparing_names(name_1, name_2):
     name_1 = name_1.lower()
     name_2 = name_2.lower()
 
+    def remove_postfix(text, postfix='(dlc)'):
+        if text.endswith(postfix):
+            text = text[:-len(postfix)]
+
+        return text
+
     # Удаление символов кроме буквенных, цифр и _: "the witcher®3:___ вася! wild hunt" -> "thewitcher3___васяwildhunt"
     def clear_name(name):
         import re
         return re.sub(r'\W', '', name)
+
+    name_1 = remove_postfix(name_1)
+    name_2 = remove_postfix(name_2)
 
     return clear_name(name_1) == clear_name(name_2)
 

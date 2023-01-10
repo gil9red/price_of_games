503d44cb5ff45e5751acad12f14b60b30fa094e8
gil9red <ilya.petrash@inbox.ru>
2023-01-10 14:56:46 +0500

    Рефакторинг

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ DIR_LOG.mkdir(parents=True, exist_ok=True)
 DB_DIR_NAME = DIR / 'database'
 DB_DIR_NAME.mkdir(parents=True, exist_ok=True)
 
-DB_FILE_NAME = str(DB_DIR_NAME / 'games.sqlite')
+DB_FILE_NAME = DB_DIR_NAME / 'games.sqlite'
 
 BACKUP_DIR_LIST = [
     DIR / 'backup',

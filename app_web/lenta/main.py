4c77cf8c50f638478814badb8e6a39faf0fae5f5
gil9red <ilya.petrash@inbox.ru>
2025-06-20 00:56:19 +0500

    Рефакторинг. app_web/lenta/main.py

--- app_web/lenta/main.py
+++ app_web/lenta/main.py
@@ -28,12 +28,12 @@ def get_year_by_number() -> list[tuple[int, int]]:
     return items
 
 
-def get_all_finished() -> list[Game]:
+def get_games() -> list[Game]:
     return list(Game.select().order_by(Game.append_date.desc()))
 
 
 def get_all_finished_by_year(year: int) -> list[Game]:
-    return [game for game in get_all_finished() if game.append_date.year == year]
+    return [game for game in get_games() if game.append_date.year == year]
 
 
 def get_day_by_games(year: int) -> dict[str, list[Game]]:
@@ -65,7 +65,7 @@ def index():
     year_by_number: list[tuple[int, int]] = get_year_by_number()
     last_year: int = year_by_number[0][0] if year_by_number else date.today().year
 
-    games: list[Game] = get_all_finished()
+    games: list[Game] = get_games()
     total_finished_game: int = len(
         [game for game in games if game.kind == FINISHED_GAME]
     )

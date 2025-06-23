4332fbae16acd4a0e0a12140cea71f943fc9bec8
gil9red <ilya.petrash@inbox.ru>
2025-06-24 00:39:03 +0500

    Рефакторинг

--- config.py
+++ config.py
@@ -10,24 +10,24 @@ from pathlib import Path
 
 DIR = Path(__file__).resolve().parent
 
-DIR_LOGS = DIR / "logs"
+DIR_LOGS: Path = DIR / "logs"
 DIR_LOGS.mkdir(parents=True, exist_ok=True)
 
-DB_DIR_NAME = DIR / "database"
+DB_DIR_NAME: Path = DIR / "database"
 DB_DIR_NAME.mkdir(parents=True, exist_ok=True)
 
-DB_FILE_NAME = DB_DIR_NAME / "games.sqlite"
+DB_FILE_NAME: Path = DB_DIR_NAME / "games.sqlite"
 
-BACKUP_DIR_LIST = [
+BACKUP_DIR_LIST: list[Path] = [
     DIR / "backup",
 ]
 if additional_backup_dir := os.getenv("ADDITIONAL_BACKUP_DIR"):
     BACKUP_DIR_LIST.append(Path(additional_backup_dir))
 
-PORT_WEB = int(
+PORT_WEB: int = int(
     os.environ.get("PORT_WEB", 5500)
 )
 
-PORT_GET_GAME_GENRES = int(
+PORT_GET_GAME_GENRES: int = int(
     os.environ.get("PORT_GET_GAME_GENRES", 5501)
 )

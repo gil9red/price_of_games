83ff3b1c973fd48b872109c78aaa338973a82d99
gil9red <ilya.petrash@inbox.ru>
2026-04-17 13:36:36 +0500

    Обновлено. price_of_games/app_parser/main.py. По-умолчанию, теперь парсер, запускается один раз, для зацикленности нужно передавать аргумент --loop

--- src/price_of_games/app_parser/main.py
+++ src/price_of_games/app_parser/main.py
@@ -11,10 +11,15 @@ __author__ = "ipetrash"
 """
 
 
+import time
+import sys
 from datetime import datetime
 
 from price_of_games.app_parser.utils import Game, get_games
-from price_of_games.app_parser.logic import append_games_to_database, fill_price_of_games
+from price_of_games.app_parser.logic import (
+    append_games_to_database,
+    fill_price_of_games,
+)
 from price_of_games.common import get_logger, FINISHED_GAME, FINISHED_WATCHED
 from price_of_games.db import Settings, db_create_backup
 from price_of_games.integrity_check import run as run_integrity_check
@@ -23,11 +28,14 @@ from price_of_games.integrator_genres.main import run as fill_genres_of_games
 # pip install simple-wait
 from simple_wait import wait
 
+IS_LOOP: bool = "--loop" in sys.argv
 
 log = get_logger("main")
 
 
 def run() -> tuple[list[int], list[int]]:
+    log.debug("Start")
+
     # Перед выполнением, запоминаем дату и время, чтобы иметь потом представление когда
     # в последний раз выполнялось заполнение списка
     Settings.set_value("last_run_date", datetime.now())
@@ -75,19 +83,43 @@ def run() -> tuple[list[int], list[int]]:
     return added_finished_game_ids, added_watched_game_ids
 
 
-if __name__ == "__main__":
+def run_loop():
     while True:
         try:
-            log.debug("Start")
-
             run()
 
-            # Every 1 hours
+            # Every 1 hour
             wait(hours=1)
 
-        except:
+        except Exception:
             log.exception("Ошибка:")
             log.debug("Через 5 минут попробую снова...")
 
             # Wait 5 minutes before next attempt
             wait(minutes=5)
+
+
+def main():
+    if IS_LOOP:
+        run_loop()
+        return
+
+    last_error: Exception | None = None
+    for _ in range(5):
+        try:
+            run()
+            return
+        except Exception as e:
+            last_error = e
+
+            log.exception("Ошибка:")
+            log.debug("Через 1 минуту попробую снова...")
+            #  NOTE: Тут без wait, т.к. оно пишет в консоль обратный отсчет
+            #        Что в некоторых случаях может быть неудобно
+            time.sleep(60)
+
+    raise last_error
+
+
+if __name__ == "__main__":
+    main()
